bb63c1de5e0f0005c4ecbe439d5bd339d83d7388
SunriseKovalevski <Sunrisekovalevsky283@gmail.com>
2020-08-29 20:14:43 +0300

    add callback into helper function

--- force-app/main/default/aura/transactionPage/transactionPageController.js
+++ force-app/main/default/aura/transactionPage/transactionPageController.js
@@ -1,4 +1,7 @@
 ({
+    init : function(component, event, helper) {
+        helper.getTransactions(component);
+    },
     myAction : function(component, event, helper) {
 
     }
--- force-app/main/default/aura/transactionPage/transactionPageHelper.js
+++ force-app/main/default/aura/transactionPage/transactionPageHelper.js
@@ -1,5 +1,15 @@
 ({
+    getTransactions : function (component) {
+        var action = component.get('c.getTransactions');
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.transactionList", response.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);
+    },
     helperMethod : function() {
-
+        
     }
 })
