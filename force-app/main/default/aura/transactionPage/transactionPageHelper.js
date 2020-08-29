8da4bc05a3cdd314430f5eff54e49197c5cd3f6f
SunriseKovalevski <Sunrisekovalevsky283@gmail.com>
2020-08-29 21:42:51 +0300

    add user func into helper and start user class func

--- force-app/main/default/aura/transactionPage/transactionPageHelper.js
+++ force-app/main/default/aura/transactionPage/transactionPageHelper.js
@@ -1,5 +1,13 @@
 ({
     getTransactions : function (component) {
+        var action1 = component.get('c.getUser');
+        action1.setCallback(this, function(response) {
+            var state1 = response.getState();
+            if (state1 === "SUCCESS") {
+                component.set("v.user", response.getReturnValue());
+            }
+        })
+        $A.enqueueAction(action1);
         var action = component.get('c.getTransactions');
         action.setCallback(this, function(response) {
             var state = response.getState();
@@ -8,6 +16,7 @@
             }
         });
         $A.enqueueAction(action);
+       
     },
     helperMethod : function() {
         
