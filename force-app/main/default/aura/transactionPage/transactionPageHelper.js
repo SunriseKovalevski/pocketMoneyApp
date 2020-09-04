f8094f9b13b78967db4b5c35934cd575ccb48ade
SunriseKovalevski <Sunrisekovalevsky283@gmail.com>
2020-09-04 15:37:57 +0300

    feat: add user creds output

--- force-app/main/default/aura/transactionPage/transactionPageHelper.js
+++ force-app/main/default/aura/transactionPage/transactionPageHelper.js
@@ -4,7 +4,8 @@
         action1.setCallback(this, function(response) {
             var state1 = response.getState();
             if (state1 === "SUCCESS") {
-                component.set("v.user", response.getReturnValue());
+                component.set('v.user', response.getReturnValue());
+                
             }
         })
         $A.enqueueAction(action1);
@@ -18,6 +19,16 @@
             }
         });
         $A.enqueueAction(action);
+
+        var action2 = component.get('c.getId');
+        action2.setCallback(this, function(response) {
+            var state2 = response.getState();
+            if (state2 === "SUCCESS") {
+                component.set('v.uID', response.getReturnValue());
+                console.log('hi hello2 => ' + component.get('v.uID'));
+            }
+        });
+        $A.enqueueAction(action2);
         
     },
     helperMethod : function() {
